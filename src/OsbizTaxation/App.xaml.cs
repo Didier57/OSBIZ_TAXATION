dfd6bf6c86cd7b04fc9738958cef48ceabcd2c21
Didier57 <didier@macchi.fr>
2026-09-25 14:50:50 +0200

    Instance unique : un seul lancement de l'application autorise (Mutex)

--- src/OsbizTaxation/App.xaml.cs
+++ src/OsbizTaxation/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Threading;
+using System.Windows;
 using OsbizTaxation.Helpers;
 using OsbizTaxation.Services;
 
@@ -6,9 +7,45 @@ namespace OsbizTaxation;
 
 public partial class App : Application
 {
+    private const string InstanceMutexName = @"Local\OsbizTaxation.SingleInstance";
+    private Mutex? _instanceMutex;
+    private bool _ownsMutex;
+
     protected override void OnStartup(StartupEventArgs e)
     {
+        _instanceMutex = new Mutex(true, InstanceMutexName, out bool createdNew);
+        _ownsMutex = createdNew;
+
+        if (!createdNew)
+        {
+            MessageBox.Show(
+                "Taxation OSBIZ est déjà ouvert. Un seul lancement est autorisé.",
+                "Taxation OSBIZ",
+                MessageBoxButton.OK,
+                MessageBoxImage.Information);
+            _instanceMutex.Dispose();
+            _instanceMutex = null;
+            Shutdown();
+            return;
+        }
+
         ThemeManager.Apply(ConfigService.Load().Theme);
         base.OnStartup(e);
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        if (_instanceMutex != null)
+        {
+            if (_ownsMutex)
+            {
+                _instanceMutex.ReleaseMutex();
+            }
+
+            _instanceMutex.Dispose();
+            _instanceMutex = null;
+        }
+
+        base.OnExit(e);
     }
 }
